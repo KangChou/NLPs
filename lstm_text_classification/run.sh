15a196ddc6f78f7878a440044e44c052425a764b
KangChou <kangchou666@gmail.com>
2022-06-11 21:05:24 +0800

    Update run.sh

--- lstm_text_classification/run.sh
+++ lstm_text_classification/run.sh
@@ -1,9 +1,21 @@
+
+#环境配置
 #pip install PyQt5==5.15.4 PyQt5-Qt5==5.15.2 PyQt5-sip==12.8.1 pyqt5-tools pyqt5-plugins matplotlib==3.2.0
 #sudo apt-get install graphviz
 #pip install keras==2.6
+#tensorflow                2.6.2
+#tensorflow-addons         0.13.0
+#tensorflow-datasets       4.4.0
+#tensorflow-estimator      2.6.0
+#tensorflow-gpu            2.5.0
+#tensorflow-hub            0.12.0
+#tensorflow-metadata       1.2.0
+#tensorflow-serving-api    2.6.2
 #pip install pydot-ng graphviz pydot pydotplus
+#sudo apt-get install -y mesa-utils libgl1-mesa-glx
+
+# 运行命令
 export DISPLAY=:0
-sudo apt-get install -y mesa-utils libgl1-mesa-glx
 export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb
 export QT_DEBUG_PLUGINS=1
 export FONTCONFIG_FILE=/etc/fonts/fonts.conf
@@ -13,6 +25,7 @@ export PYTHONIOENCODING=utf-8;python train.py
 export PYTHONIOENCODING=utf-8;python predicts.py
 #终端的输出信息保存到log中:bash run.sh >& screen.log
 
+# 记录
 #<<comment
 #Requirement already satisfied: PyQt5-Qt5==5.15.2 in /opt/conda/lib/python3.6/site-packages (5.15.2)
 #Collecting PyQt5-sip==12.8.1
